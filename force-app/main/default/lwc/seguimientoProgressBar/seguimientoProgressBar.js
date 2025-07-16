7bb127b27e8a1acd3cf5ac23242c24e6ef6fb922
David Gonzales <david25bmx.dg@gmail.com>
2025-07-16 10:57:19 -0500

    Mejora del componente LWC

--- force-app/main/default/lwc/seguimientoProgressBar/seguimientoProgressBar.js
+++ force-app/main/default/lwc/seguimientoProgressBar/seguimientoProgressBar.js
@@ -1,10 +1,15 @@
 import { LightningElement, api, wire } from 'lwc';
 import getSeguimientos from '@salesforce/apex/SeguimientoController.getSeguimientos';
+import getResumenSeguimientos from '@salesforce/apex/SeguimientoController.getResumenSeguimientos';
 
 export default class SeguimientoProgressBar extends LightningElement {
+    
     @api recordId;
+    
     total = 0;
     completados = 0;
+    resumen = {};
+    hayResumen = false;
 
     get porcentaje() {
         return this.total > 0 ? Math.round((this.completados / this.total) * 100) : 0;
@@ -13,7 +18,7 @@ export default class SeguimientoProgressBar extends LightningElement {
     get haySeguimientos() {
         return this.total > 0;
     }
-
+    
     @wire(getSeguimientos, { contactoId: '$recordId' })
     wiredSeguimientos({ error, data }) {
         if (data) {
@@ -21,6 +26,17 @@ export default class SeguimientoProgressBar extends LightningElement {
             this.completados = data.filter(seg => seg.Etapa__c === 'Completado').length;
         } else if (error) {
             console.error('Error al obtener seguimientos:', error);
+        }
+    }
+
+    @wire(getResumenSeguimientos, { contactoId: '$recordId' })
+    wiredResumen({ error, data }) {
+        if (data) {
+            this.resumen = data;
+            this.hayResumen = true;
+        } else if (error) {
+            console.error('Error al obtener el resumen de seguimientos:', error);
+            this.hayResumen = false;
         }
     }
 }
